3d5baaca32ce6b9db171e2c1c99febacc9a55f62
Ryochi <ryo1.t.zubu@gmail.com>
2021-12-11 17:07:15 +1000

    started home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { Alert, Button, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import "../style/Home.css";
+import { Alert, Button, Card, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
@@ -19,18 +20,28 @@ const Home = () => {
     }
     
     return (
-        <>
-            <Card>
-                <Card.Body>
-                    <h2>Something</h2>
-                </Card.Body>
-            </Card>
-            <div>
-                {error && <Alert variant='danger'>{error}</Alert>}
-                <Button variant='link' onClick={handleLogout}>Sign Out</Button>
+        <div className="home-container">
+            <div className="float-left">
+                <div className="title-container">
+                    <Card.Text className="text-center title-font" style={{color: "#fff1c1"}}>TeamPro</Card.Text>
+                </div>
+                <div className="logout-container">
+                    {error && <Alert variant='danger'>{error}</Alert>}
+                    <Button variant="outline-secondary" className="signout-btn" onClick={handleLogout}>Sign Out</Button>
+                </div>
             </div>
-        </>
-    )
+            <div className="float-right ">
+                <Container className="d-flex align-items-center justify-content-center" >
+                <Card className="chat-card">
+                    <Card.Body>
+                    <h1>somthing</h1>
+                    </Card.Body>
+                </Card>
+                </Container>
+            </div>
+            
+        </div>
+    );
 }
 
 export default Home;
