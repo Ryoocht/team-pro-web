eca8c55f937b588e3dc51517af52df5104946bfe
Ryochi <ryo1.t.zubu@gmail.com>
2021-12-16 08:43:43 +1000

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+// firebase.initializeApp ({
+//     messagingSenderId: "963647731245"
+// });
+
+// const messaging = firebase.messaging();
