98668f47a470feb7662232383ccf237ab2296d5b
Ryochi <ryo1.t.zubu@gmail.com>
2021-12-23 19:35:32 +1000

    Fixed Router

--- src/components/Title.js
+++ src/components/Title.js
@@ -10,7 +10,7 @@ const Title = () => {
             <Card.Body className="text-center card-background">
                 <Card.Text className="title-font" style={{color: "#fff1c1"}}>TeamPro</Card.Text>
                 <div className="d-grid gap-2">
-                    <Link to="/signup" className="title-btn signup-btn"><span>Sign Up</span></Link>
+                    <Link to="/team-pro-web/signup" className="title-btn signup-btn"><span>Sign Up</span></Link>
                     <Link to="/login" className="title-btn login-btn"><span>Log In</span></Link>
                 </div>
             </Card.Body>
