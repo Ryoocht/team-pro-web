88f015253e1126d060edc70198827e8ff1c5f93f
Ryochi <ryo1.t.zubu@gmail.com>
2021-12-23 19:30:59 +1000

    Fixed routers

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../style/Title.css";
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Title = () => {
 
@@ -9,8 +10,8 @@ const Title = () => {
             <Card.Body className="text-center card-background">
                 <Card.Text className="title-font" style={{color: "#fff1c1"}}>TeamPro</Card.Text>
                 <div className="d-grid gap-2">
-                    <a href="/team-pro-web/signup" className="title-btn signup-btn"><span>Sign Up</span></a>
-                    <a href="/team-pro-web/login" className="title-btn login-btn"><span>Log In</span></a>
+                    <Link to="/signup" className="title-btn signup-btn"><span>Sign Up</span></Link>
+                    <Link to="/login" className="title-btn login-btn"><span>Log In</span></Link>
                 </div>
             </Card.Body>
         </Card>
